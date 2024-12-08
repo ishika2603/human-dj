62026913afc41e96d070a9eacd7026955efed63f
Eva Schiller <eva_schiller@brown.edu>
2024-12-08 16:58:19 -0500

    capstone_stencil

---
+++ humandj/onboard_player.ino
@@ -0,0 +1,21 @@
+/*
+* Library to convert MIDI notes to waveforms and send to onboard player.
+* Authored by Eva and Komron for HumanDJ capstone component.
+*/
+
+
+void convert_sound(int* midi_states, int* fader_states) {
+  return;
+}
+
+void send_onboard_note(int frequency) {
+  return;
+}
+
+void stop_onboard_note() {
+  return;
+}
+
+void onboard_player(int* touch_states, int* midi_states, int* fader_states) {
+  return;
+}
